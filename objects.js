f41d1d2e978e9d8a3dae710bfe7b2fdda3c66f6d
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-08-29 14:43:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {names: 'song titles'};
 function updatePlaylist (playlist,names,songtitle) {
-  return Object.assign({},playlist, {'Alison', 'Slowdive'})
+  return Object.assign({},playlist, {Alison: ['Slowdive']})
 }
